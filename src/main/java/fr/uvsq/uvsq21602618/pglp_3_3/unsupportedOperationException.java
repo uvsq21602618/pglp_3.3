97a4f6003910b7cd8e85fcf7fd83bbde70191b0c
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-01 12:38:10 +0100

    ajout methode avance dans RobotStatique ainsi que la creation de la classe unsupportedExceptionOperation

---
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/unsupportedOperationException.java
@@ -0,0 +1,8 @@
+package fr.uvsq.uvsq21602618.pglp_3_3;
+
+public class unsupportedOperationException extends Exception {
+
+	public unsupportedOperationException (String message) {
+		super(message);
+	}
+}
