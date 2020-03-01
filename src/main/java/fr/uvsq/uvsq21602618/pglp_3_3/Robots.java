7fb9908f06b6df32288736a8cda5b1159f06a669
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-01 12:45:05 +0100

    ajouts méthode avancerTous dans Robots

--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robots.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robots.java
@@ -2,6 +2,8 @@ package fr.uvsq.uvsq21602618.pglp_3_3;
 
 import java.util.ArrayList;
 
+import fr.uvsq.uvsq21602618.Pglp_3_2.Employe;
+
 public class Robots {
 	private ArrayList<Robot> listeRobots;
 	
@@ -11,5 +13,13 @@ public class Robots {
 	
 	public void addRobot(Robot R) {
 		this.listeRobots.add(R);
+	}
+	
+	public void avancerTous() throws unsupportedOperationException {
+		if(!listeRobots.isEmpty()) {
+			for(Robot R: listeRobots) {
+				R.avance();
+			}
+		}
 	}
 }
