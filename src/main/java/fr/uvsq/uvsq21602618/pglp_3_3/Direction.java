3a439dd5ea09c1b96e8085f3062fee64e7ef780e
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-01 12:22:49 +0100

    Ajout constructeur et méthode tourne dans Robot

--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Direction.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Direction.java
@@ -1,8 +1,8 @@
 package fr.uvsq.uvsq21602618.pglp_3_3;
 
 public enum Direction {
-	DROITE,
-	GAUCHE,
-	HAUT,
-	BAS;
+	NORD,
+	SUD,
+	OUEST,
+	EST;
 }
--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robot.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robot.java
@@ -3,5 +3,30 @@ package fr.uvsq.uvsq21602618.pglp_3_3;
 public class Robot {
 	private Position position;
 	private Direction direction;
+	
+	public Robot(Position pos, Direction dir) {
+		this.position=pos;
+		this.direction=dir;
+	}
+	
+	public void tourne() {
+		if(direction.equals(Direction.NORD)) {
+			this.direction=Direction.EST;
+		}
+		else if(direction.equals(Direction.EST)) {
+			this.direction=Direction.SUD;
+		}
+		else if(direction.equals(Direction.SUD)) {
+			this.direction=Direction.OUEST;
+		}
+		else {
+			this.direction=Direction.NORD;
+		}
+		
+	}
+	
+	public void avance() {
+		
+	}
 
 }
