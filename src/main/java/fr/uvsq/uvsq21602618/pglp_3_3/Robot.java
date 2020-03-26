14fed5e5b18f3ab4c48164714f9600ab1a19f9fc
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-03-26 21:38:31 +0100

    suppression de methodes en double

--- src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robot.java
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_3/Robot.java
@@ -5,14 +5,6 @@ package fr.uvsq.uvsq21602618.pglp_3_3;
  *
  */
 public class Robot extends RobotStatique {
-    /**
-     * Position du Robot.
-     */
-    private Position position;
-    /**
-     * Direction du Robot.
-     */
-    private Direction direction;
 	/**
 	 * Constructeur du Robot.
 	 * @param pos La position en x et y
@@ -51,33 +43,4 @@ public class Robot extends RobotStatique {
 		    super.setPosition(super.getPosition().deplaceX(-1));
 		}
 	}
-	
-	   /**
-     * Méthode pour récupérer la direction du robot.
-     * @return la direction
-     */
-    public Direction getDirection() {
-        return this.direction;
-    }
-    /**
-     * Méthode pour définir la direction du robot.
-     * @param dir L'orientation
-     */
-    public void setDirection(Direction dir) {
-        this.direction = dir;
-    }
-    /**
-     * Méthode pour récupérer la position du robot.
-     * @return la position
-     */
-    public Position getPosition() {
-        return this.position;
-    }
-    /**
-     * Méthode pour définir la position du robot.
-     * @param pos la position
-     */
-    public void setPosition(Position pos) {
-        this.position = pos;
-    }
 }
