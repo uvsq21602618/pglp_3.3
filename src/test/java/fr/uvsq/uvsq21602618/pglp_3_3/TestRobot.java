49c23994aa06ee8be60370b8ace849c3a9a52750
nathalie bernard <nathalie.bernard@ens.uvsq.fr>
2020-05-24 20:52:01 +0200

    Derniere verification

--- src/test/java/fr/uvsq/uvsq21602618/pglp_3_3/TestRobot.java
+++ src/test/java/fr/uvsq/uvsq21602618/pglp_3_3/TestRobot.java
@@ -13,7 +13,7 @@ public class TestRobot {
 	 * Test de la méthode tourne.
 	 */
 	@Test
-	public void tourneTest() {
+	public void tourneTestEst() {
 		Position pos = new Position(2, 3);
 		Direction dir = Direction.NORD;
 		Robot R = new Robot(pos, dir);
@@ -22,13 +22,51 @@ public class TestRobot {
 		
 		assertEquals(expected, R.getDirection());
 	}
-	/*
+	/**
+     * Test de la méthode tourne.
+     */
+    @Test
+    public void tourneTestSud() {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.EST;
+        Robot R = new Robot(pos, dir);
+        Direction expected = Direction.SUD;
+        R.tourne();
+        
+        assertEquals(expected, R.getDirection());
+    }
+    /**
+     * Test de la méthode tourne.
+     */
+    @Test
+    public void tourneTestOuest() {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.SUD;
+        Robot R = new Robot(pos, dir);
+        Direction expected = Direction.OUEST;
+        R.tourne();
+        
+        assertEquals(expected, R.getDirection());
+    }
+    /**
+     * Test de la méthode tourne.
+     */
+    @Test
+    public void tourneTestNord() {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.OUEST;
+        Robot R = new Robot(pos, dir);
+        Direction expected = Direction.NORD;
+        R.tourne();
+        
+        assertEquals(expected, R.getDirection());
+    }
 	/**
 	 * Test de la méthode avance.
 	 * @throws unsupportedOperationException
-	 *
+	 */
 	@Test
-	public void avanceTest() throws unsupportedOperationException {
+	public void avanceTestNord() /*throws unsupportedOperationException*/ {
 		Position pos = new Position(2, 3);
 		Direction dir = Direction.NORD;
 		Robot r = new Robot(pos, dir);
@@ -37,5 +75,51 @@ public class TestRobot {
 		
 		assertEquals(expected.getY(), r.getPosition().getY());
 		assertEquals(expected.getX(), r.getPosition().getX());
-	}*/
+	}
+	/**
+     * Test de la méthode avance.
+     * @throws unsupportedOperationException
+     */
+    @Test
+    public void avanceTestEst() /*throws unsupportedOperationException*/ {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.EST;
+        Robot r = new Robot(pos, dir);
+        Position expected = new Position(3, 3);
+        r.avance();
+        
+        assertEquals(expected.getY(), r.getPosition().getY());
+        assertEquals(expected.getX(), r.getPosition().getX());
+    }
+    /**
+     * Test de la méthode avance.
+     * @throws unsupportedOperationException
+     */
+    @Test
+    public void avanceTestSud() /*throws unsupportedOperationException*/ {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.SUD;
+        Robot r = new Robot(pos, dir);
+        Position expected = new Position(2, 2);
+        r.avance();
+        
+        assertEquals(expected.getY(), r.getPosition().getY());
+        assertEquals(expected.getX(), r.getPosition().getX());
+    }
+    /**
+     * Test de la méthode avance.
+     * @throws unsupportedOperationException
+     */
+    @Test
+    public void avanceTestOuest() /*throws unsupportedOperationException*/ {
+        Position pos = new Position(2, 3);
+        Direction dir = Direction.OUEST;
+        Robot r = new Robot(pos, dir);
+        Position expected = new Position(1, 3);
+        r.avance();
+        
+        assertEquals(expected.getY(), r.getPosition().getY());
+        assertEquals(expected.getX(), r.getPosition().getX());
+    }
+
 }
